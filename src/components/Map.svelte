a87911123d58c9f1ca870b74d5c2f0efb1444501
john gravois <jagravois@gmail.com>
2020-12-30 17:12:47 -0800

    add leaflet to the mix

---
+++ src/components/Map.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from 'svelte';
+
+  // no 'window' server-side
+	onMount(async () => {
+		const L = await import('leaflet')
+
+    const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+	  const attribution = '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+
+		const map = L.map('map').setView([37.75, -122.23], 10)
+		L.tileLayer(url, { attribution }).addTo(map)
+	});
+</script>
+
+<style></style>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+
+<div id="map" style="height:400px;width:100%"></div>
---
+++ src/routes/map.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Map from '../components/Map.svelte';
+</script>
+
+<svelte:head>
+	<title>Map</title>
+</svelte:head>
+
+<Map></Map>
+
+
